9cfc4442d9f800eb4eb8474b60543c9bf0f64080
Arvikdb17 <128912243+Arvikdb17@users.noreply.github.com>
2024-09-19 20:22:40 +0530

    Add files via upload

---
+++ Basics/MultipleClassSingleFile.java
@@ -0,0 +1,14 @@
+package Basics;
+
+class MultipleClassSingleFile {
+    //Create multiple classes in single file and compile and explore how many .class files are generated.
+   /* public static void main(String[] args){
+      System.out.println("a");
+    }*/
+}
+class A{
+
+}
+class B{
+
+}
